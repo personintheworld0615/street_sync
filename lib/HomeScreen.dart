fc28355a263345e8d3596c4727348fed3d14bb76
Aarav Garg <aaravg.0615@gmail.com>
2026-07-07 14:28:34 -0400

    header done

--- lib/HomeScreen.dart
+++ lib/HomeScreen.dart
@@ -10,8 +10,86 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: SizedBox.shrink(),
+    return Scaffold(
+      body: Column(
+        children: [
+          header(),
+          Container(
+            padding: EdgeInsets.fromLTRB(10, 30, 30, 10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Card(
+                  elevation: 8,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(16)
+                  ),
+                  // child: Padding(padding: EdgeInsets.all(10),
+                  //   child: ),
+                ),
+
+              ],
+            ),
+          )
+        ],
+      ),
     );
+  }
+
+  Container header() {
+    return Container(
+          width: double.infinity,
+          color: Colors.blue,
+          padding: const EdgeInsets.fromLTRB(24, 60, 24, 32),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'StreetSync',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontStyle: FontStyle.italic,
+                  color: Colors.white,
+                ),
+              ),
+              const SizedBox(height: 5),
+              Text(
+                'Good morning Aarav',
+                style: TextStyle(
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+              const SizedBox(height: 10),
+              Container(
+                padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                decoration: BoxDecoration(
+                  color: Colors.white.withOpacity(0.2),
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    const Icon(
+                      Icons.near_me_outlined,
+                      color: Colors.white,
+                      size: 20,
+                    ),
+                    const SizedBox(width: 8),
+                    Text(
+                      'West Windsor, NJ',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 14,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        );
   }
 }
