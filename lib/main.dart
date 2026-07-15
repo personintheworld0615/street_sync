2b9b11a7402aa4415663855f1116ce782bcdf18c
KrishExists <krishworld432@gmail.com>
2026-07-14 22:18:11 -0400

    Added a profiledart screen, empty

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:street_sync/HomeScreen.dart';
+import 'package:street_sync/Leaderboard.dart';
+import 'package:street_sync/Profile.dart';
 
 void main() {
   runApp(
     const MaterialApp(
-      home: HomeScreen(),
+      home: ProfileScreen(),
     ),
   );
 }
