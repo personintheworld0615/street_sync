149d0850bbce49d6d30462d7ae5a984157750b9c
Aarav Garg <aaravg.0615@gmail.com>
2026-07-17 10:08:48 -0400

    added user locaiton

--- lib/Map.dart
+++ lib/Map.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MapScreen extends StatefulWidget {
@@ -9,20 +10,58 @@ class MapScreen extends StatefulWidget {
 }
 
 class _MapScreenState extends State<MapScreen> {
+  GoogleMapController? _controller;
+  LatLng _center = const LatLng(40.3573, -74.6672);
+  bool _ready = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _goToUser();
+  }
+
+  Future<void> _goToUser() async {
+    var permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+    }
+    if (permission == LocationPermission.denied ||
+        permission == LocationPermission.deniedForever) {
+      setState(() => _ready = true);
+      return;
+    }
+
+    final currentLocation = await Geolocator.getCurrentPosition();
+    final userLatLng = LatLng(
+      currentLocation.latitude,
+      currentLocation.longitude,
+    );
+
+    setState(() {
+      _center = userLatLng;
+      _ready = true;
+    });
+
+    await _controller?.animateCamera(
+      CameraUpdate.newLatLngZoom(userLatLng, 15),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Map'),
-      ),
+      appBar: AppBar(title: const Text('Map')),
       body: GoogleMap(
-        initialCameraPosition: const CameraPosition(
-          target: LatLng(40.3573, -74.6672),
-          zoom: 14,
-        ),
-        myLocationButtonEnabled: false,
-        zoomControlsEnabled: false,
-        mapType: MapType.normal,
+        initialCameraPosition: CameraPosition(target: _center, zoom: 14),
+        myLocationEnabled: true,
+        myLocationButtonEnabled: true,
+        zoomControlsEnabled: true,
+        onMapCreated: (c) async {
+          _controller = c;
+          if (_ready) {
+            await c.animateCamera(CameraUpdate.newLatLngZoom(_center, 15));
+          }
+        },
       ),
     );
   }
